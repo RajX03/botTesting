b9cdbbea778a8c8f34256ab33ed26d1085ea698a
Raj Patil <157887214+RajX03@users.noreply.github.com>
2026-07-13 10:37:39 +0530

    Simplify navigateTo function
    Removed SalesIQ reset logic and delay before navigation.

--- common.js
+++ common.js
@@ -1,16 +1,3 @@
 function navigateTo(page) {
-
-    // Reset SalesIQ conversation
-    if (window.$zoho &&
-        $zoho.salesiq &&
-        typeof $zoho.salesiq.reset === "function") {
-
-        $zoho.salesiq.reset();
-    }
-
-    // Small delay so reset finishes
-    setTimeout(function () {
         window.location.href = page;
-    }, 1000);
-
 }
